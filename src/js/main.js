00163e4b78fc27d425a1a642179c5f97d2a68653
Volodymyr Tyshchenko <volodymyr.tyshchenko@gmail.com>
2021-08-29 18:15:31 +0300

    all works

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,5 @@
-// console.log(menu);
+import menuData from '../menu.json';
+import menuTemplate from '../templates/card.hbs';
 
 const Theme = {
   LIGHT: 'light-theme',
@@ -13,9 +14,13 @@ if (!uiTheme) {
 
 const body = document.getElementsByTagName('body')[0];
 const changeThemeCheckbox = document.getElementById('theme-switch-toggle');
+const menu = document.querySelector('.js-menu');
 
 body.classList.add(uiTheme);
 changeThemeCheckbox.checked = uiTheme === Theme.LIGHT ? false : true;
+
+const markup = menuTemplate(menuData);
+menu.insertAdjacentHTML('beforeend', markup);
 
 changeThemeCheckbox.addEventListener('click', onChangeTheme);
 
