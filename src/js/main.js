78067f8d3cdf82cc96511acc4f53f4649ffde92a
Volodymyr Tyshchenko <volodymyr.tyshchenko@gmail.com>
2021-08-29 15:25:42 +0300

    bugfix

---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+// console.log(menu);
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const body = document.getElementsByTagName('body');
+const changeThemeCheckbox = document.getElementsByName('theme');
+console.log(changeThemeCheckbox);
