3c4238dd2261692edab651e1c71ef7677ffa1dec
Volodymyr Tyshchenko <volodymyr.tyshchenko@gmail.com>
2021-08-29 17:21:02 +0300

    ui-thme is working

--- src/js/main.js
+++ src/js/main.js
@@ -5,6 +5,22 @@ const Theme = {
   DARK: 'dark-theme',
 };
 
-const body = document.getElementsByTagName('body');
-const changeThemeCheckbox = document.getElementsByName('theme');
-console.log(changeThemeCheckbox);
+let uiTheme = localStorage.getItem('ui-theme');
+if (!uiTheme) {
+  uiTheme = Theme.LIGHT;
+  localStorage.setItem('ui-theme', Theme.LIGHT);
+}
+
+const body = document.getElementsByTagName('body')[0];
+const changeThemeCheckbox = document.getElementById('theme-switch-toggle');
+
+body.classList.add(uiTheme);
+changeThemeCheckbox.checked = uiTheme === Theme.LIGHT ? false : true;
+
+changeThemeCheckbox.addEventListener('click', onChangeTheme);
+
+function onChangeTheme(event) {
+  body.classList.toggle(Theme.DARK);
+  body.classList.toggle(Theme.LIGHT);
+  localStorage.setItem('ui-theme', event.target.checked ? Theme.DARK : Theme.LIGHT);
+}
